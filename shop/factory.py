5e2734e38e721d2a65f12259cb08d5dd1003e436
Thiago Ferreira <th.thiago96@gmail.com>
2022-08-29 18:51:20 -0300

    fix: improved models and factories

--- shop/factory.py
+++ shop/factory.py
@@ -1,6 +1,8 @@
 import factory
-from shop.models import Customer, ProductCategory, Order, Product, OrderItem
 import factory.fuzzy
+
+from shop.models import Customer, Order, OrderItem, Product, ProductCategory
+
 
 class CustomerFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -10,12 +12,14 @@ class CustomerFactory(factory.django.DjangoModelFactory):
     birth_date = factory.Faker("date")
     country = factory.Iterator(["US", "BR", "AR"])
 
+
 class ProductCategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = ProductCategory
 
     name = factory.Faker("name")
-    minumum_age_allowed = factory.Iterator([21, 18, 18, 0, 0 ,0])
+    minumum_age_allowed = factory.Iterator([21, 18, 18, 0, 0, 0])
+
 
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -24,22 +28,26 @@ class ProductFactory(factory.django.DjangoModelFactory):
     name = factory.Faker("name")
     description = factory.Faker("sentence")
     category = factory.SubFactory(ProductCategoryFactory)
-    price = factory.fuzzy.FuzzyDecimal(0.5, 999)
+    price = factory.fuzzy.FuzzyDecimal(0.5, 15)
+
 
 class OrderFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Order
-    
+
     customer = factory.SubFactory(CustomerFactory)
     order_date = factory.Faker("date")
     is_paid = factory.Iterator([True, False])
-    delivery_method = factory.Iterator(map(lambda x: x[0], Order.delivery_method.field.choices))
+    delivery_method = factory.Iterator(
+        map(lambda x: x[0], Order.delivery_method.field.choices)
+    )
     total = 0
+
 
 class OrderItemFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = OrderItem
-    
+
     order = factory.SubFactory(OrderFactory)
     product = factory.SubFactory(ProductFactory)
     amount = factory.fuzzy.FuzzyDecimal(0.1, 10)
--- shop/models.py
+++ shop/models.py
@@ -24,13 +24,17 @@ class ProductCategory(BaseModel):
 class Product(BaseModel):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+    category = models.ForeignKey(
+        ProductCategory, on_delete=models.PROTECT, related_name="products"
+    )
     price = models.DecimalField(max_digits=16, decimal_places=2)
     active = models.BooleanField(default=True)
 
 
 class Order(BaseModel):
-    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.PROTECT, related_name="orders"
+    )
     order_date = models.DateField()
     is_paid = models.BooleanField(default=False)
     delivery_method = models.CharField(
@@ -43,10 +47,20 @@ class Order(BaseModel):
     )
     total = models.DecimalField(max_digits=16, decimal_places=2)
 
+    def _calculate_total(self):
+        self.total = self.items.all().aggregate(models.Sum("subtotal"))["subtotal__sum"]
+
+    def save(self, *args, should_calculate_total=False, **kwargs):
+        if should_calculate_total:
+            self._calculate_total()
+        return super().save(*args, **kwargs)
+
 
 class OrderItem(BaseModel):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name="items")
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name="items_sold"
+    )
     amount = models.DecimalField(max_digits=16, decimal_places=2)
     unit_price = models.DecimalField(max_digits=16, decimal_places=2)
     subtotal = models.DecimalField(max_digits=16, decimal_places=2)
@@ -54,6 +68,6 @@ class OrderItem(BaseModel):
     def save(self, *args, **kwargs):
         if not self.unit_price:
             self.unit_price = self.product.price
-            
-        self.subtotal = self.unit_price * self.amount    
+
+        self.subtotal = self.unit_price * self.amount
         return super().save(*args, **kwargs)
