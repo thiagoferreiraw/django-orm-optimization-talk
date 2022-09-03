7a9d779ec5f0ba706d0e927f83f1eee86c77879b
Thiago Ferreira <th.thiago96@gmail.com>
2022-09-03 11:26:35 -0300

    feat: new queries and enhancements

--- shop/helpers.py
+++ shop/helpers.py
@@ -3,22 +3,32 @@ from functools import wraps
 from django.db import connection, reset_queries
 
 
-def debug_queries(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        reset_queries()
-        func(*args, **kwargs)
-        _print_sql_statements()
-
-    return wrapper
+class DebugTypes:
+    FULL = "full"
+    SHORT = "short"
 
 
-def _print_sql_statements():
+def debug_queries(print_mode=DebugTypes.SHORT):
+    def inner_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            reset_queries()
+            func(*args, **kwargs)
+            _print_sql_statements(print_mode)
+
+        return wrapper
+
+    return inner_decorator
+
+
+def _print_sql_statements(print_mode):
     print(f"\n{'-'*60}\nSQL statements:\n")
     for idx, query in enumerate(connection.queries, start=1):
-        print(idx, _get_sql_statement_without_fields(query["sql"]), "\n")
+        print(idx, _get_formatted_sql_statement(query["sql"], print_mode), "\n")
     print(f"\n{'-'*60}")
 
 
-def _get_sql_statement_without_fields(sql):
-    return sql[sql.find("FROM") :]
+def _get_formatted_sql_statement(sql, print_mode):
+    if print_mode == DebugTypes.SHORT:
+        return sql[sql.find("FROM") :]
+    return sql
--- shop/samples.py
+++ shop/samples.py
@@ -1,26 +1,26 @@
-from sys import prefix
+from django.db.models import Avg, Count, Max, Min, Sum
 
 from django.db.models import Prefetch
 
-from shop.helpers import debug_queries
-from shop.models import Order, OrderItem
+from shop.helpers import debug_queries, DebugTypes
+from shop.models import Customer, Order, OrderItem
 
 
-@debug_queries
+@debug_queries()
 def list_orders_bad(limit=5):
     orders = Order.objects.filter()[:limit]
     for order in orders:
         print(f"Order #{order.id} - {order.customer.name} - ${order.total}")
 
 
-@debug_queries
+@debug_queries()
 def list_orders_good(limit=5):
     orders = Order.objects.filter().select_related("customer")[:limit]
     for order in orders:
         print(f"Order #{order.id} - {order.customer.name} - ${order.total}")
 
 
-@debug_queries
+@debug_queries()
 def list_order_items_bad(order_id=1):
     order = Order.objects.get(id=order_id)
     print(f"Printing Order #{order.id} - {order.customer.name}", "\n", "-" * 60)
@@ -30,7 +30,7 @@ def list_order_items_bad(order_id=1):
         print("Subtotal: ", item.subtotal, "\n")
 
 
-@debug_queries
+@debug_queries()
 def list_order_items_good(order_id=1):
     order = Order.objects.select_related("customer").get(id=order_id)
     print(f"Printing Order #{order.id} - {order.customer.name}", "\n", "-" * 60)
@@ -40,7 +40,7 @@ def list_order_items_good(order_id=1):
         print("Subtotal: ", item.subtotal, "\n")
 
 
-@debug_queries
+@debug_queries()
 def list_multiple_orders_and_items_bad(limit=5):
     orders = Order.objects.filter()[:limit]
     for order in orders:
@@ -51,7 +51,7 @@ def list_multiple_orders_and_items_bad(limit=5):
             print("Subtotal: ", item.subtotal, "\n")
 
 
-@debug_queries
+@debug_queries()
 def list_multiple_orders_and_items_medium(limit=5):
     orders = Order.objects.select_related("customer").filter()[:limit]
     for order in orders:
@@ -62,7 +62,7 @@ def list_multiple_orders_and_items_medium(limit=5):
             print("Subtotal: ", item.subtotal, "\n")
 
 
-@debug_queries
+@debug_queries()
 def list_multiple_orders_and_items_good(limit=5):
     prefetch_items = Prefetch(
         "items", queryset=OrderItem.objects.select_related("product__category")
@@ -78,3 +78,38 @@ def list_multiple_orders_and_items_good(limit=5):
             print("Product: ", item.product.name)
             print("Category: ", item.product.category.name)
             print("Subtotal: ", item.subtotal, "\n")
+
+
+@debug_queries(DebugTypes.FULL)
+def list_order_totals():
+    order_stats = Order.objects.aggregate(
+        Avg("total"), Max("total"), Min("total"), Sum("total"), count_orders=Count("id")
+    )
+    for key, value in order_stats.items():
+        print(f"{key} => {value}")
+
+
+@debug_queries(DebugTypes.FULL)
+def list_top_paying_customers(limit=5):
+    top_customers = (
+        Customer.objects.annotate(
+            orders_placed=Count("orders"), total_value=Sum("orders__total")
+        )
+        .order_by("-total_value")
+        .values_list("name", "orders_placed", "total_value")[:limit]
+    )
+    for name, orders_placed, total_value in top_customers:
+        print("Customer: ", name)
+        print("Orders placed: ", orders_placed)
+        print("Total Value: ", total_value)
+
+
+def run_all():
+    from shop import samples
+
+    for name, sample in samples.__dict__.items():
+        if not name.startswith("list_"):
+            continue
+
+        print(f"======== {name} ==========")
+        sample()
