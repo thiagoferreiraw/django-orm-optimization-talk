057381cac0a257a8d9bb4727f25f6b78f11aff7c
Thiago Ferreira <th.thiago96@gmail.com>
2022-09-05 18:03:15 -0300

    feat: include sql timer

--- shop/helpers.py
+++ shop/helpers.py
@@ -1,3 +1,4 @@
+import time
 from functools import wraps
 
 from django.db import connection, reset_queries
@@ -13,16 +14,23 @@ def debug_queries(print_mode=DebugTypes.SHORT):
         @wraps(func)
         def wrapper(*args, **kwargs):
             reset_queries()
+            start_time = time.perf_counter()
             func(*args, **kwargs)
-            _print_sql_statements(print_mode)
+            time_elapsed_ms = (time.perf_counter() - start_time) * 1000
+            _print_sql_statements(print_mode, time_elapsed_ms)
 
         return wrapper
 
     return inner_decorator
 
 
-def _print_sql_statements(print_mode):
-    print(f"\n{'-'*60}\n[{len(connection.queries)}] SQL statements executed:\n")
+def _print_sql_statements(print_mode, time_elapsed_ms):
+    total_sql_time_ms = sum(map(lambda q: float(q["time"]), connection.queries)) * 1000
+    print(
+        f"\n{'-'*60}\n[{len(connection.queries)}] SQL statements executed "
+        f"(Total time = {time_elapsed_ms:0.1f}ms, "
+        f"SQL time = {total_sql_time_ms:0.1f}ms):\n"
+    )
     for idx, query in enumerate(connection.queries, start=1):
         print(idx, _get_formatted_sql_statement(query["sql"], print_mode), "\n")
     print(f"\n{'-'*60}")
